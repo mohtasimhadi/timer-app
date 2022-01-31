2b1c7883ee898d72d1e2697bf026efc672a9de57
MOHTASIM HADI RAFI <mohtasimhadi@gmail.com>
2022-01-31 11:23:06 +0600

    Cart initials done

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,18 +1,59 @@
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, Form } from "react-bootstrap";
+import { useState } from "react";
 
 export default function Cart(props) {
-    return (
+  const [editButton, setEditButton] = useState(false);
+  const [cartData, setCartData] = useState({
+    title: props.title,
+    subtitle: props.subtitle,
+  });
+
+  const { title, subtitle } = cartData;
+
+  function onClickEdit() {
+    setEditButton(true);
+  }
+
+  function onClickSubmit() {
+      setEditButton(false)
+  }
+
+  function onChangeForm(e) {
+    setCartData({ ...cartData, [e.target.name]: e.target.value})
+  }
+
+  return (
+    <>
+      {!editButton && (
         <Card>
-            <Card.Title>{props.title}</Card.Title>
-            <Card.Body>
-                <p>{props.subtitle}</p>
-                <p>time</p>
-                <Button>Delete</Button>
-                <Button>Edit</Button>
-            </Card.Body>
-            <Card.Footer>
-                <Button>Start</Button>
-            </Card.Footer>
+          <Card.Title>{title}</Card.Title>
+          <Card.Body>
+            <p>{subtitle}</p>
+            <p>time</p>
+            <Button>Delete</Button>
+            <Button onClick={onClickEdit}>Edit</Button>
+          </Card.Body>
+          <Card.Footer>
+            <Button>Start</Button>
+          </Card.Footer>
         </Card>
-    )
+      )}
+      {editButton && (
+        <Form>
+          <Form.Group className="mb-3" controlId="title">
+            <Form.Label>Title</Form.Label>
+            <Form.Control type="text" placeholder={title} name='title' onChange={onChangeForm} />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="subtitle">
+            <Form.Label>Subtitle</Form.Label>
+            <Form.Control type="text" placeholder={subtitle} name='subtitle' onChange={onChangeForm} />
+          </Form.Group>
+          <Button variant="primary" type="submit" onClick={onClickSubmit}>
+            Submit
+          </Button>
+        </Form>
+      )}
+    </>
+  );
 }
